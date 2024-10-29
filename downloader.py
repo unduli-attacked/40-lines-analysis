848bf310bf349e9e645db22cbd18ac484f2d1d6f
Jay <45951442+unduli-attacked@users.noreply.github.com>
2024-10-29 14:46:34 -0600

    attempt at user record data

--- downloader.py
+++ downloader.py
@@ -3,10 +3,13 @@ import random
 import json
 import time
 import math
+import pandas as pd
 
 # define static vars
 api_base = "https://ch.tetr.io/api"
 leaderboard_endpoint = "/records/40l_global" # global 40 lines leaderboard
+user_info_endpoint = "/users"
+user_records_endpoint = "/records/40l" # APPEND TO USER INFO
 speed_limiter = 1 # time to sleep between requests, in seconds
 
 def getLeaderboard(num_records, page_size=100):
@@ -102,6 +105,55 @@ def getLeaderboard(num_records, page_size=100):
         if speed_limiter > (pgEnd - pgStart):
             # speed limit has not been met, sleep for remaining time
             time.sleep(speed_limiter - (pgEnd - pgStart))
+
+def processRecord(record_json, user_id):
+    results = record_json["results"]
+    # FIXME this is stupid
+    record_row = [record_json["_id"], user_id, record_json["ts"], record_json["pb"], record_json["oncepb"], results["finaltime"], results["aggregatestats"]["pps"], results["stats"]["inputs"], results["stats"]["score"]]
+    return record_row
+
+
+def getUserData(leaderboard_file, top_rank, bottom_rank):
+    info_df = pd.DataFrame(columns=["id", "username", "rank", "country", "created_date", "xp", "achievement_rating", "TL_games_played", "TL_games_won", "TL_play_time", "records_progression", "records_recent"])
+    records_df = pd.DataFrame(columns=["record_id", "user_id", "datetime", "current_pb", "former_pb", "final_time", "pps", "inputs", "score", "pieces_placed", "singles", "doubles", "triples", "quads", "all_clears", "finesse_faults", "finesse_perf"])
+    
+    leaderboard = pd.read_csv(leaderboard_file)
+    leaderboard = leaderboard.loc[leaderboard["rank"] >= bottom_rank]
+    leaderboard = leaderboard.loc[leaderboard["rank"] <= top_rank]
+
+    for row in leaderboard:
+        info_req = api_base+ user_info_endpoint+"/"+row["user_id"]
+        prog_req = info_req+user_records_endpoint+"/progression"
+        recent_req = info_req+user_records_endpoint+"/recent"
+
+        user_info = requests.get(info_req)
+        user_prog = requests.get(prog_req)
+        recent_req = requests.get(recent_req)
+
+        usrStart = time.time()
+        
+        if (user_info.status_code == 200):
+            # user info data retrieved
+            user_info_data = user_info.json()["data"]
+            info_df.loc[len(info_df)] = [user_info_data["_id"], user_info_data["username"], row["rank"], user_info_data["country"], user_info_data["ts"], user_info_data["xp"], user_info_data["ar"], user_info_data["gamesplayed"], user_info_data["gameswon"], user_info_data["gametime"], [], []]
+
+            if user_prog.status_code == 200:
+                # user progression data retrieved
+                prog_data = user_prog.json()["data"]["entries"]
+
+                for rec in prog_data:
+                    records_df[len(records_df)] = processRecord(rec, row["user_id"])
+        
+        
+        # record end time
+        usrEnd = time.time()        
+
+        # determine if the speed limit has been met
+        if speed_limiter*3 > (usrEnd - usrStart):
+            # speed limit has not been met, sleep for remaining time
+            time.sleep(speed_limiter*3 - (usrEnd - usrStart))
+
+        
 
 
 # TODO call this with page size 100 and infinite record limit to get data on the full leaderboard
