c25e1f27478d19a00ae2d6b2d3558b8cf59adb1c
Jay <45951442+unduli-attacked@users.noreply.github.com>
2024-11-03 13:51:58 -0700

    make the leaderboard downloader suck less

--- downloader.py
+++ downloader.py
@@ -14,11 +14,12 @@ leaderboard_endpoint = "/records/40l_global" # global 40 lines leaderboard
 user_info_endpoint = "/users"
 user_records_endpoint = "/records/40l" # APPEND TO USER INFO
 speed_limiter = 1 # time to sleep between requests, in seconds
+session_id = "CSCI4502_"+str(random.randbytes(16).hex()) # TODO check that this actually works
+headers = {"X-Session-ID":session_id}
 
-def getLeaderboard(num_records, page_size=100):
-    session_id = "CSCI4502_"+str(random.randbytes(16).hex()) # TODO check that this actually works
-    headers = {"X-Session-ID":session_id}
-    
+
+# download the leaderboard
+def getLeaderboard(num_records=math.inf, page_size=100):
     # check inputs
     if num_records % page_size != 0:
         print("num_records must be divisible by page_size")
@@ -27,20 +28,14 @@ def getLeaderboard(num_records, page_size=100):
         print("invalid page size")
         return None
     
+    leaderboard_df = pd.DataFrame(columns=["user_id", "user_name", "rank", "final_time", "record_id", "prisecter"])
+
     # initialize output files
     out_summ_name = "out/user_leaderboard_"+str(math.floor(time.time()))+".csv"
-    out_all_name = "out/full_leaderboard_"+str(math.floor(time.time()))+".json"
-
-    out_summ = open(out_summ_name, "w")
-    out_summ.write("user_id,user_name,rank,final_time,record_id,pri,sec,ter\n") # CSV header
-    out_summ.close()
-
-    out_all = open(out_all_name, "w")
-    out_all.write("[\n") # start of JSON array
-    out_all.close()
     
     # prepare to loop
-    last_prisecter = {}
+    last_prisecter = ""
+    limit = str(page_size)
     rec_count = 0
     fail = False
 
@@ -51,52 +46,35 @@ def getLeaderboard(num_records, page_size=100):
         if last_prisecter:
             # pass in the last prisecter for pagination
             req_str += "after="+last_prisecter+"&"
-        req_str+="limit="+str(page_size)
+        req_str+="limit="+limit
 
         # get page
         resp = requests.get(req_str, headers=headers)
-        respPage = resp.json()
 
         # get start time for speed limit
         pgStart = time.time()
 
+        respPage = resp.json()
+
         # check if data was returned
-        if respPage["success"] == True:
-            # assemble the summary from the returned data
-            summStr = ""
-            
+        if respPage["success"] == True and len(respPage["data"]["entries"]) > 0:
+            # add returned records to the dataframe
+
             # iterate through each record on the page
             for i in respPage["data"]["entries"]:
-                rec_count+=1 # inc record count
+                rec_count += 1
 
-                # assemble the summary string for the CSV
-                # yes it probably would've been smart to use pandas for this but by the time I thought of that I was committed
-                summStr += i["user"]["id"]+","+i["user"]["username"]+","+str(rec_count)+","+str(i["results"]["stats"]["finaltime"])+","+i["_id"]+","+str(i["p"]["pri"])+","+str(i["p"]["sec"])+","+str(i["p"]["ter"])+"\n"
-                
-                # check if this is the last record on the page
-                if rec_count % page_size == 0:
-                    # record the prisecter of the last record for the next query
-                    # it needs to be in the format pri:sec:ter or it'll throw an error
-                    last_prisecter = str(i["p"]["pri"])+":"+str(i["p"]["sec"])+":"+str(i["p"]["ter"])
-            
-            # write summary data
-            try:
-                summFl = open(out_summ_name, "a")
-                summFl.write(summStr)
-                summFl.close()
-            except:
-                print("summary write error")
+                last_prisecter = str(i["p"]["pri"])+":"+str(i["p"]["sec"])+":"+str(i["p"]["ter"])
+                rec_row = {
+                    "user_id": i["user"]["id"],
+                    "user_name": i["user"]["username"],
+                    "rank": rec_count,
+                    "final_time": i["results"]["stats"]["finaltime"],
+                    "record_id": i["_id"],
+                    "prisecter": last_prisecter
+                }
 
-            # write full data (json)
-            try:
-                fullFl = open(out_all_name, "a+")
-                fullStr = json.dumps(respPage["data"]["entries"]) # get the entries array
-                fullStr = fullStr.strip('[]') # remove the brackets to make a frankenarray
-                fullFl.write(fullStr) # write to file
-                fullFl.write(",") # add comma
-                fullFl.close()
-            except:
-                print("full write error")
+                leaderboard_df = pd.concat([leaderboard_df, pd.DataFrame(rec_row, index=[0])], ignore_index=True)
         else:
             # no records were returned, something is wrong or we've hit the bottom
             fail=True
@@ -108,6 +86,10 @@ def getLeaderboard(num_records, page_size=100):
         if speed_limiter > (pgEnd - pgStart):
             # speed limit has not been met, sleep for remaining time
             time.sleep(speed_limiter - (pgEnd - pgStart))
+    
+    # write the leaderboard to a file
+    out_summ_fl = open(out_summ_name, "w")
+    leaderboard_df.to_csv(out_summ_fl)
 
 def processRecord(record_json, user_id):
     results = record_json["results"]
@@ -139,7 +121,7 @@ def getUserRecords(records_df, recent_req, user_id, last_prisecter=""):
     if last_prisecter != "":
         req_url += "&after="+last_prisecter
     
-    user_recent = requests.get(req_url)
+    user_recent = requests.get(req_url, headers=headers)
     pgStart = time.time()
 
     if user_recent.status_code == 200:
@@ -160,13 +142,11 @@ def getUserRecords(records_df, recent_req, user_id, last_prisecter=""):
     return getUserRecords(records_df, recent_req, user_id, last_prisecter)
 
 def getUserData(leaderboard_file, best_rank, worst_rank):
-    session_id = "CSCI4502_"+str(random.randbytes(16).hex()) # TODO check that this actually works
-    headers = {"X-Session-ID":session_id}
 
     info_df = pd.DataFrame(columns=["id", "username", "rank", "country", "created_date", "xp", "achievement_rating", "TL_games_played", "TL_games_won", "TL_play_time", "num_records"])
     records_df = pd.DataFrame(columns=["record_id", "user_id", "datetime", "current_pb", "ever_pb", "final_time", "pps", "inputs", "score", "pieces_placed", "singles", "doubles", "triples", "quads", "all_clears", "finesse_faults", "finesse_perf"])
     
-    leaderboard = pd.read_csv(leaderboard_file)
+    leaderboard = pd.read_csv(leaderboard_file, index_col=0)
     # print(leaderboard.info())
     leaderboard = leaderboard.loc[leaderboard["rank"] <= worst_rank]
     # print(leaderboard.info())
@@ -175,18 +155,18 @@ def getUserData(leaderboard_file, best_rank, worst_rank):
 
     for i in leaderboard.index:
         row = leaderboard.loc[i]
-        # print(row)
+        print(row)
         info_req = api_base+ user_info_endpoint+"/"+row["user_id"]
         recent_req = info_req+user_records_endpoint+"/recent?limit=100"
 
-        user_info = requests.get(info_req)
+        user_info = requests.get(info_req, headers=headers)
 
         usrStart = time.time()
         
         if user_info.status_code == 200:
             # user info data retrieved
             user_info_data = user_info.json()["data"]
-            info_df.loc[len(info_df)] = [user_info_data["_id"], user_info_data["username"], row["rank"], user_info_data["country"], user_info_data["ts"], user_info_data["xp"], user_info_data["ar"], user_info_data["gamesplayed"], user_info_data["gameswon"], user_info_data["gametime"], None, None]
+            info_df.loc[len(info_df)] = [user_info_data["_id"], user_info_data["username"], row["rank"], user_info_data["country"], user_info_data["ts"], user_info_data["xp"], user_info_data["ar"], user_info_data["gamesplayed"], user_info_data["gameswon"], user_info_data["gametime"], 0]
             
             # record end time
             usrEnd = time.time()        
@@ -217,4 +197,4 @@ def getUserData(leaderboard_file, best_rank, worst_rank):
 # getLeaderboard(20,10)
 
 # TODO iterate through user list and download summary data as well as individual game records (progression?)
-getUserData("out/user_leaderboard_1730174943.csv", 1, 2)
+getUserData("out/user_leaderboard_1730666309.csv", 1, 5)
